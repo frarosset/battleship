8a8194ea0bbff81bd00789a141bb13735f09a848
frarosset <37275581+frarosset@users.noreply.github.com>
2024-10-16 01:55:54 +0200

    Add size properties definition to Gameboard class to make the previous test pass

--- src/js-modules/logic/Gameboard.js
+++ src/js-modules/logic/Gameboard.js
@@ -1,1 +1,23 @@
-export default class Gameboard {}
+import Cell from "./Cell.js";
+
+export default class Gameboard {
+  #nCols;
+  #nRows;
+
+  constructor(nCols, nRows = nCols) {
+    this.#nCols = nCols;
+    this.#nRows = nRows;
+  }
+
+  get size() {
+    return [this.#nCols, this.#nRows];
+  }
+
+  get nCols() {
+    return this.#nCols;
+  }
+
+  get nRows() {
+    return this.#nRows;
+  }
+}
