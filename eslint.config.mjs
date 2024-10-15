660c7a4744ac5381f8f245e7e936fe3a9cd2b488
frarosset <37275581+frarosset@users.noreply.github.com>
2024-10-15 17:40:42 +0200

    Configure ESLint to import Jest global helpers

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,14 +2,23 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   {
     rules: {
-      "prefer-const": ["error", { "ignoreReadBeforeAssign": true }]
-    }
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+    },
   },
   eslintConfigPrettier,
+  {
+    overrides: [
+      {
+        files: ["tests/**/*"],
+        env: {
+          jest: true,
+        },
+      },
+    ],
+  },
 ];
