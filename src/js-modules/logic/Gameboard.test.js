306aa947df9ee45209109c56a041f42c887d24b3
frarosset <37275581+frarosset@users.noreply.github.com>
2024-10-18 11:35:53 +0200

    Add failing test: Gameboard instances have a fleet property that can be used to retrieve current ships

--- src/js-modules/logic/Gameboard.test.js
+++ src/js-modules/logic/Gameboard.test.js
@@ -49,4 +49,8 @@ describe("Gameboard class", () => {
       )
     );
   });
+
+  it("has a fleet property that can be used to retrieve current ships", () => {
+    expect(gameboard.fleet).toEqual([]);
+  });
 });
