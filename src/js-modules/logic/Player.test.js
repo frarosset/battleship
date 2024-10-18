f202e414b7b802c00d1d6cc163e17d9232e71b4c
frarosset <37275581+frarosset@users.noreply.github.com>
2024-10-19 01:27:40 +0200

    Add failing test: Player instances have a gameboard that can be retrieved

--- src/js-modules/logic/Player.test.js
+++ src/js-modules/logic/Player.test.js
@@ -1,14 +1,20 @@
 import Player from "./Player.js";
+import Gameboard from "./Gameboard.js";
 
 describe("Player class", () => {
   it("is defined", () => {
     expect(Player).toBeDefined();
   });
 
+  const sizeGameboard = 10;
   const playerName = "Captain X";
-  const player = new Player(playerName);
+  const player = new Player(playerName, sizeGameboard);
 
   it("has a name", () => {
     expect(player.name).toBe(playerName);
+  });
+
+  it("has a gameboard", () => {
+    expect(player.gameboard).toEqual(new Gameboard(sizeGameboard));
   });
 });
