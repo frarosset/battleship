7109c884dcfbf23cd9a54ffe5b3d9cdf02f422cc
frarosset <37275581+frarosset@users.noreply.github.com>
2024-10-19 03:57:22 +0200

    Add failing test: AiPlayer insatances have the characteristics of normal Player

--- src/js-modules/logic/AiPlayer.test.js
+++ src/js-modules/logic/AiPlayer.test.js
@@ -5,4 +5,32 @@ describe("AiPlayer class", () => {
   it("is defined", () => {
     expect(AiPlayer).toBeDefined();
   });
+
+  const playerName = "Captain AI";
+  const sizeGameboard = 10;
+  const fleet = [
+    ["Carrier", 5],
+    ["Battleship", 4],
+    ["Cruiser", 3],
+    ["Submarine", 3],
+    ["Destroyer", 2],
+  ];
+  const fleetNames = fleet.map((item) => item[0]);
+
+  const aiPlayer = new AiPlayer(playerName, fleet, sizeGameboard);
+
+  it("has the characteristics of a normal Player", () => {
+    // copy the same tests as Player class
+    // has a name
+    expect(aiPlayer.name).toBe(playerName);
+    //has a gameboard
+    expect(aiPlayer.gameboard).toEqual(new Gameboard(sizeGameboard));
+    // has a fleet
+    expect(aiPlayer.gameboard.fleet).toEqual(fleetNames);
+    expect(aiPlayer.gameboard.notDeployedFleet).toEqual(fleetNames);
+    // it can randomly place the (not deployed) fleet in the board
+    aiPlayer.randomShipsPlacement();
+    expect(aiPlayer.gameboard.fleet).toEqual(fleetNames);
+    expect(aiPlayer.gameboard.deployedFleet).toEqual(fleetNames);
+  });
 });
