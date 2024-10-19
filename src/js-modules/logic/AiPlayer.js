da272c5a67b8f1c12bec6e981c837d0c0d141262
frarosset <37275581+frarosset@users.noreply.github.com>
2024-10-19 04:01:50 +0200

    Make AiPlayer inherit from Player class to pass the previous test

--- src/js-modules/logic/AiPlayer.js
+++ src/js-modules/logic/AiPlayer.js
@@ -1,1 +1,12 @@
-export default class AiPlayer {}
+import Player from "./Player.js";
+
+export default class AiPlayer extends Player {
+  constructor(
+    name,
+    fleet = Player.defaultFleet,
+    nColsGameboard = Player.defaultSizeGameboard,
+    nRowsGameboard = nColsGameboard
+  ) {
+    super(name, fleet, nColsGameboard, nRowsGameboard);
+  }
+}
