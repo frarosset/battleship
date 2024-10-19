31d54fe2979b5c4d80eba46323e8ffe0b680a13e
frarosset <37275581+frarosset@users.noreply.github.com>
2024-10-19 02:09:41 +0200

    Initialize Player instances with a (not deployed) fleet that can be retrieved to pass the previous test

--- src/js-modules/logic/Player.js
+++ src/js-modules/logic/Player.js
@@ -1,12 +1,28 @@
 import Gameboard from "./Gameboard.js";
 
+const defaultSizeGameboard = 10;
+const defaultFleet = [
+  ["Carrier", 5],
+  ["Battleship", 4],
+  ["Cruiser", 3],
+  ["Submarine", 3],
+  ["Destroyer", 2],
+];
+
 export default class Player {
   #name;
   #gameboard;
 
-  constructor(name, nColsGameboard = 10, nRowsGameboard = nColsGameboard) {
+  constructor(
+    name,
+    fleet = defaultFleet,
+    nColsGameboard = defaultSizeGameboard,
+    nRowsGameboard = nColsGameboard
+  ) {
     this.#name = name;
     this.#gameboard = new Gameboard(nColsGameboard, nRowsGameboard);
+
+    fleet.forEach((item) => this.#gameboard.addShip(...item));
   }
 
   get name() {
