b1fce5e4badc9d0090376f2755081ac3833f409f
frarosset <37275581+frarosset@users.noreply.github.com>
2024-10-20 16:15:31 +0200

    Add CellDom module to create Cell divs to be used in GameboardDom instances
    A grid display is forced for GameboardDom.div, and each cell is
    opportunely placed based on the coordinates.

---
+++ src/js-modules/dom/CellDom.js
@@ -0,0 +1,37 @@
+import { initDiv } from "../../js-utilities/commonDomComponents";
+
+const blockName = "cell";
+const cssClass = {};
+const getCssClass = (element) => `${blockName}__${cssClass[element]}`;
+
+export default class CellDom {
+  #div;
+  #cell;
+
+  constructor(cell) {
+    this.#cell = cell;
+    this.#div = initCellDiv(cell);
+    this.#div.obj = this;
+  }
+
+  // getters
+  get div() {
+    return this.#div;
+  }
+
+  get cell() {
+    return this.#cell;
+  }
+}
+
+// private methods
+// if they use 'this', they have to be evoked as: methodName.call(this,args)
+
+function initCellDiv(cell) {
+  const div = initDiv(blockName);
+  div.style.gridCol = cell.x + 1;
+  div.style.gridRow = cell.y + 1;
+  div.style.aspectRatio = 1;
+  div.style.border = "1px solid darkblue"; //temporary
+  return div;
+}
