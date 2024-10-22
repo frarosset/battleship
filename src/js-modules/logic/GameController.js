538752aae8dd0856c1aa58d6f5f094ff109ad2ad
frarosset <37275581+frarosset@users.noreply.github.com>
2024-10-22 18:02:37 +0200

    Make GameController class initialize players (human or ai) that can be retrieved to pass the previous test

--- src/js-modules/logic/GameController.js
+++ src/js-modules/logic/GameController.js
@@ -1,1 +1,28 @@
-export default class GameController {}
+import Player from "./Player.js";
+import AiPlayer from "./AiPlayer.js";
+
+export default class GameController {
+  #player1;
+  #player2;
+
+  constructor(player1Name, player2Name, versusAi = true) {
+    // create the players
+    this.#player1 = this.#initPlayer(player1Name, false);
+    this.#player2 = this.#initPlayer(player2Name, versusAi);
+  }
+
+  get player1() {
+    return this.#player1;
+  }
+  get player2() {
+    return this.#player2;
+  }
+
+  #initPlayer(name, isAi = false) {
+    if (isAi) {
+      return new AiPlayer(name);
+    } else {
+      return new Player(name);
+    }
+  }
+}
