ae81d17d87330ae105d049e2df4f427ded9a954a
frarosset <37275581+frarosset@users.noreply.github.com>
2024-10-31 15:32:58 +0100

    Add style to .players-name page

--- src/js-modules/dom/PlayersNameViewDom.js
+++ src/js-modules/dom/PlayersNameViewDom.js
@@ -85,7 +85,7 @@ export default class PlayersNameViewDom {
     const playersCntDiv = initDiv(getCssClass("playersCntDiv"));
 
     const player1Div = this.#initPlayerNameDiv(defaultData.player1);
-    const vsP = initP(getCssClass("vsP"), null, "vs");
+    const vsP = initP(getCssClass("vsP"), null, "VS");
     const player2Div = this.#versusAi
       ? this.#initAiPlayerNameDiv(defaultData.aiPlayer)
       : this.#initPlayerNameDiv(defaultData.player2);
