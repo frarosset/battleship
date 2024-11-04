b291817b8bdbbd3ab64f4d9d6818d3860cf10756
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-05 00:34:18 +0100

    Remove some unused modules from js-utilities + improve removeDescendants() function from commonDomUtilities

--- src/js-modules/initWebpage.js
+++ src/js-modules/initWebpage.js
@@ -4,7 +4,7 @@ import GameViewDom from "./dom/GameViewDom.js";
 import GameEndViewDom from "./dom/GameEndViewDom.js";
 import PubSub from "pubsub-js";
 import { pubSubTokens } from "./pubSubTokens.js";
-import { removeDescendants } from "../js-utilities/commonDomUtilities.js";
+import { resetContent } from "../js-utilities/commonDomUtilities.js";
 
 const container = document.body;
 
@@ -21,9 +21,9 @@ export default function initWebpage() {
 
 function renderGameViewDom(token, { player1, player2, versusAi }) {
   console.log(`${token} - ${player1.name} vs ${player2.name}`);
-  removeDescendants(container);
   const gameViewDom = new GameViewDom(player1, player2, versusAi);
-  container.append(gameViewDom.div);
+  resetContent(container, gameViewDom.div);
+
   // Notify the initialization of the page
   PubSub.publish(pubSubTokens.gameViewInitialized);
 }
@@ -33,26 +33,23 @@ function renderGameEndViewDom(
   { winnerPlayerName, defeatedPlayerName, versusAi, isWinnerAi }
 ) {
   console.log(`${token} - ${winnerPlayerName} wins over ${defeatedPlayerName}`);
-  removeDescendants(container);
   const gameEndViewDom = new GameEndViewDom(
     winnerPlayerName,
     defeatedPlayerName,
     versusAi,
     isWinnerAi
   );
-  container.append(gameEndViewDom.div);
+  resetContent(container, gameEndViewDom.div);
 }
 
 function renderHomeViewDom(token) {
   console.log(`${token}`);
-  removeDescendants(container);
   const homeViewDom = new HomeViewDom();
-  container.append(homeViewDom.div);
+  resetContent(container, homeViewDom.div);
 }
 
 function renderPlayersNameViewDom(token, { versusAi }) {
   console.log(`${token}`);
-  removeDescendants(container);
   const playersNameViewDom = new PlayersNameViewDom(versusAi);
-  container.append(playersNameViewDom.div);
+  resetContent(container, playersNameViewDom.div);
 }
