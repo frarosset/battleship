3cc63f3fa6c13dbc19a942e28642edd41a9ddfc9
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-05 17:38:18 +0100

    Add HTML structure to DeployFleetViewDom div
    Note that the ships are not shown yet. Also, the randomize button is
    not working, yet, too. Finally, if the player is AI, a different dom
    structure should be used. All of these will be implemented later.

--- src/js-modules/dom/DeployFleetViewDom.js
+++ src/js-modules/dom/DeployFleetViewDom.js
@@ -1,23 +1,31 @@
-import { initDiv } from "../../js-utilities/commonDomComponents.js";
+import {
+  initDiv,
+  initP,
+  initButton,
+  initHeader,
+} from "../../js-utilities/commonDomComponents.js";
+import PlayerDom from "./PlayerDom.js";
 import { pubSubTokens } from "../pubSubTokens.js";
 import PubSub from "pubsub-js";
 
 const blockName = "deploy-fleet";
-const cssClass = {};
+const cssClass = {
+  header: "header",
+  playerDiv: "player-div",
+  btns: "btns",
+  msgP: "msg-p",
+};
 const getCssClass = (element) => `${blockName}__${cssClass[element]}`;
 
 export default class DeployFleetViewDom {
   #div;
+  #playerDom;
 
   constructor(player, isAi) {
     player.randomShipsPlacement();
+    this.#playerDom = new PlayerDom(player);
 
-    console.log(`${player.name}${isAi ? " (AI)" : ""} has deployed its fleet`);
-    // todo
     this.#div = this.#initDeployFleetViewDiv();
-
-    // Notify the initialization of the page
-    PubSub.publish(pubSubTokens.fleetDeployed);
   }
 
   // getters
@@ -28,8 +36,52 @@ export default class DeployFleetViewDom {
   #initDeployFleetViewDiv() {
     const div = initDiv(blockName);
 
-    // todo
+    const header = this.#initHeader();
+
+    const playerDiv = initDiv(getCssClass("playerDiv"));
+
+    playerDiv.append(this.#playerDom.div);
+
+    const msgP = this.#initGameMsg();
+
+    div.append(header, playerDiv, msgP);
 
     return div;
+  }
+
+  #initHeader() {
+    const header = initHeader(getCssClass("header"));
+
+    const buttonsDiv = initDiv(getCssClass("btns"));
+    const showFleetBtn = this.#initRandomizeFleetButton();
+    const fleetReadyBtn = this.#initFleetReadyButton();
+
+    buttonsDiv.append(showFleetBtn, fleetReadyBtn);
+    header.append(buttonsDiv);
+
+    return header;
+  }
+
+  #initGameMsg() {
+    // for now show a temporary msg, to setup the page...the actual message selection is todo
+    const msg = "A message to show game status (TODO)";
+    return initP(getCssClass("msgP"), null, msg);
+  }
+
+  #initRandomizeFleetButton() {
+    const btn = initButton("btn", () => {}, null, "Randomize");
+
+    return btn;
+  }
+
+  #fleetReadyCallback() {
+    // Notify the initialization of the page
+    PubSub.publish(pubSubTokens.fleetDeployed);
+  }
+
+  #initFleetReadyButton() {
+    const btn = initButton("btn", this.#fleetReadyCallback, null, "I'M READY!");
+
+    return btn;
   }
 }
